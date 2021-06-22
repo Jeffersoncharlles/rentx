ff902ea5313993de2c437e5cf2a93a7306f73978
jeffersoncharlles <contato@jefferdeveloper.com>
2021-06-22 15:34:15 -0300

    declare theme extends global commit

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import AppLoading from 'expo-app-loading';
+import {ThemeProvider} from 'styled-components';
 import {
   useFonts,
   Inter_400Regular,
@@ -11,6 +12,7 @@ import {
   Archivo_600SemiBold
 } from '@expo-google-fonts/archivo';
 import { Home } from './src/pages/Home';
+import theme from './src/styles/theme';
 
 
 
@@ -26,9 +28,10 @@ export default function App() {
     if (!fontsLoaded) {
       return <AppLoading />
     }
-
   return (
+    <ThemeProvider theme={theme}>
       <Home />
+    </ThemeProvider>
     )
 }
 
