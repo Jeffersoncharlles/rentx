699953bcb71d41797c26b521004f6af43574c90a
jeffersoncharlles <contato@jefferdeveloper.com>
2021-06-23 14:09:44 -0300

    create button back + start cardetails commit

--- App.tsx
+++ App.tsx
@@ -11,7 +11,7 @@ import {
   Archivo_500Medium,
   Archivo_600SemiBold
 } from '@expo-google-fonts/archivo';
-import { Home } from './src/pages/Home';
+import { CarDetails } from './src/pages/CarDetails';
 import theme from './src/styles/theme';
 
 
@@ -30,7 +30,7 @@ export default function App() {
     }
   return (
     <ThemeProvider theme={theme}>
-      <Home />
+      <CarDetails />
     </ThemeProvider>
     )
 }
