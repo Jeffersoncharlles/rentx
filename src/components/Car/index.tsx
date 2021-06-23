60398c996e0d78303905b8e7cb8df30282ed1c97
jeffersoncharlles <contato@jefferdeveloper.com>
2021-06-23 13:20:25 -0300

    card sem types commit

--- src/components/Car/index.tsx
+++ src/components/Car/index.tsx
@@ -18,21 +18,22 @@ export const Car = () => {
     return (
         <Container>
             <Details>
-                <Brand>Audio</Brand>
+                <Brand>Audi</Brand>
                 <Name>Rs 5 Coupe</Name>
+           
+                <About>
+                    <Rent>
+                        <Period>Ao dia</Period>
+                        <Price>R$ 120</Price>
+                    </Rent>
+
+                    <Type>
+                        <GasolineSvg />
+                    </Type>
+                </About>
             </Details>
-            <About>
-                <Rent>
-                    <Period>Ao dia</Period>
-                    <Price>R$ 120</Price>
-                </Rent>
 
-                <Type>
-                    <GasolineSvg />
-                </Type>
-            </About>
-
-            <CarImage source={{uri: ''}} />
+            <CarImage source={{uri: 'https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red-thumb.png'}} />
         </Container>
     );
 }
