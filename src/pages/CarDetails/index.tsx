d0a72a98324e9be2e0f8585e55424efc2f50660e
jeffersoncharlles <contato@jefferdeveloper.com>
2021-06-24 10:42:57 -0300

    import cardetails ImageSlider commit

--- src/pages/CarDetails/index.tsx
+++ src/pages/CarDetails/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {BackButton} from '../../components/BackButton';
+import {ImageSlider} from '../../components/ImageSlider';
 
 import {
     Container,
@@ -12,6 +13,9 @@ export const CarDetails = () => {
             <Header>
                 <BackButton onPress={()=>{}}/>
             </Header>
+            <ImageSlider 
+                imagesUrl={['https://freepngimg.com/thumb/porsche/1-2-porsche-free-png-image-thumb.png']} 
+            />
          </Container>
     );
 }
