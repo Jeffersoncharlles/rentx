faa3359ed66a3153caadf6bed597b32d1beeb156
jeffersoncharlles <contato@jefferdeveloper.com>
2021-07-01 13:40:28 -0300

    custom calendar locate  commit

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
 
-import  { Calendar as CustomCalendar, CalendarList, Agenda} from 'react-native-calendars';
+import  { Calendar as CustomCalendar, LocaleConfig, Agenda} from 'react-native-calendars';
 import { useTheme } from 'styled-components';
 
 import {
     Container,
 } from './styles';
+
+LocaleConfig.locales['pt-br'] = {
+    monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+    monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'],
+    dayNames: ['Domingo', 'Segunda','Terça', 'Quarta', 'Quinta','Sexta','Sábado'],
+    dayNamesShort: ['DOM','SEG','TER','QUA','QUI','SEX','SAB'],
+    today: 'Hoje'
+}
 
 export const Calendar = () => {
     const theme = useTheme();
@@ -17,9 +25,27 @@ export const Calendar = () => {
                 renderArrow={(direction)=> 
                     <Feather 
                         size={24}
-                        color={theme.colors.main}
+                        color={theme.colors.text}
+                        name={direction == 'left' ? "chevron-left" : "chevron-right"}
                     />
                 }
+
+                headerStyle={{
+                    backgroundColor: theme.colors.background_secondary,
+                    borderBottomWidth: 0.5,
+                    borderBottomColor:theme.colors.text_details,
+                    paddingBottom:10,
+                    marginBottom:10
+                }}
+
+                theme={{
+                    textDayFontFamily: theme.fonts.primary_400,
+                    textDayHeaderFontFamily: theme.fonts.primary_500,
+                    textDayHeaderFontSize:10,
+                    arrowStyle: {
+                        marginHorizontal: -15
+                    }
+                }}
 
             />
          </Container>
