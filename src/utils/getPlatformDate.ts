0faa573ae805bc540b8d54c1eb65171668c176d2
jeffersoncharlles <contato@jefferdeveloper.com>
2021-07-10 20:37:22 -0300

    calendar  marked refactory commit

--- src/utils/getPlatformDate.ts
+++ src/utils/getPlatformDate.ts
@@ -2,11 +2,5 @@ import { addDays } from 'date-fns';
 import {Platform} from 'react-native';
 
 export const getPlatformDate = (date: Date) =>{
-    // if (Platform.OS === 'ios') {
-    //    return  addDays(date,1) ;
-    // }else{
-    //     return  addDays(date,1);
-    // }
-
     return  Platform.OS === 'ios' ?  addDays(date,1) :  addDays(date,1) ;
-}
+} 
