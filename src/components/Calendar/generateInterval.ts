f5a071947b7b73c0b69912610cb919e575ada5ce
jeffersoncharlles <contato@jefferdeveloper.com>
2021-07-12 22:21:29 -0300

    remove coment commit

--- src/components/Calendar/generateInterval.ts
+++ src/components/Calendar/generateInterval.ts
@@ -9,19 +9,17 @@ export const generateInterval = (start: DayProps, end: DayProps)=>{
 
    eachDayOfInterval({start:new Date(start.timestamp), end: new Date(end.timestamp)}).forEach((item)=>{
        const date = format(getPlatformDate(item), 'yyy-MM-dd');
+        interval = {
+            ...interval,
+            [date]:{
+                color: start.dateString === date || end.dateString === date ?
+                theme.colors.main : theme.colors.main_light,
 
-       interval = {
-           ...interval,
-           [date]:{
-               color: start.dateString === date || end.dateString === date ?
-               theme.colors.main : theme.colors.main_light,
-
-               textColor: start.dateString === date || end.dateString === date ?
-               theme.colors.main_light : theme.colors.main,
-           }
-       }
+                textColor: start.dateString === date || end.dateString === date ?
+                theme.colors.main_light : theme.colors.main,
+            }
+        }
    });
 
    return interval;
 }
-
