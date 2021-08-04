cd76d34b75f8dc5929f9273d7cd8bb624d9eee65
Jefferson Charlle <contato@jefferdeveloper.com>
2021-08-04 16:05:19 -0300

    slide position slide ok

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { FlatList } from 'react-native';
+import { useState } from 'react';
+import { useRef } from 'react';
+import { FlatList, ViewToken } from 'react-native';
 
 import {
     Container,
@@ -13,7 +15,23 @@ interface Props {
     imagesUrl: string[];
 }
 
+interface ChangeImageProps {
+    viewableItems: ViewToken[];
+    changed: ViewToken[];
+}
+
 export const ImageSlider = ({imagesUrl}:Props) => {
+    const [imageIndex, setImageIndex] = useState(0);
+
+
+    const indexChanged = useRef((info:ChangeImageProps)=> {
+        //pego o info dentro o item na primeira posição e pegar o index exclamação para nao ser null
+        const index = info.viewableItems[0].index!;
+       setImageIndex(index);
+    }); 
+
+    
+
     return (
         <Container>
             <ImageIndexes>
@@ -21,7 +39,7 @@ export const ImageSlider = ({imagesUrl}:Props) => {
                     imagesUrl.map((item,index)=>(
                         <ImageIndex 
                             key={String(index)}
-                            active={true} 
+                            active={index === imageIndex} 
                         />
                     ))
                     
@@ -43,6 +61,7 @@ export const ImageSlider = ({imagesUrl}:Props) => {
                     )}
                     horizontal
                     showsHorizontalScrollIndicator={false}
+                   onViewableItemsChanged={indexChanged.current}
                 />
                 
             
