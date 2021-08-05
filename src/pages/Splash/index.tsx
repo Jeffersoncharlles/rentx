cc7cb76bc9c1091ebe10866ba6135a333fad0a5f
Jefferson Charlle <contato@jefferdeveloper.com>
2021-08-05 19:37:14 -0300

    Splash  animation transition

--- src/pages/Splash/index.tsx
+++ src/pages/Splash/index.tsx
@@ -1,27 +1,38 @@
 import React from 'react';
-import { Button , StyleSheet} from 'react-native';
+import { Button , StyleSheet, Dimensions} from 'react-native';
 
 
-import Animated, {useSharedValue, useAnimatedStyle} from 'react-native-reanimated';
+import Animated, {useSharedValue, useAnimatedStyle,withTiming, Easing} from 'react-native-reanimated';
 
 import {
     Container,
 } from './styles';
 
+const WIDTH = Dimensions.get('window').width;
+
 export const Splash = () => {
     const animation = useSharedValue(0);//compartilhar valores na animação
 
      //para animar o comportamento visual em tela
-     //e animatestyle e basicamente um objeto de estilo igual stylesheet
+     //e animateStyle e basicamente um objeto de estilo igual stylesheet
+     //eixo x lados eixo y cima baixo
     const animatedStyles = useAnimatedStyle(()=>{
         return{
-            transform: [{translateX: animation.value}]
+            transform: [
+                {
+                translateX: withTiming(animation.value,{ 
+                        duration:3000,
+                        easing:Easing.bounce
+                    }
+                    )
+                }
+            ]
         }
     });
    
 
     const handleAnimationPosition = ()=>{
-        animation.value = Math.random()*400;
+        animation.value = Math.random() * (WIDTH - 100);
     }
 
     return(
