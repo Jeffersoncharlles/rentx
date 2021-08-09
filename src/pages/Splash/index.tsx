43384afae6ac82e1fd398a89aefd3c402be980ae
Jefferson Charlle <contato@jefferdeveloper.com>
2021-08-08 22:05:20 -0300

    Splash start animated

--- src/pages/Splash/index.tsx
+++ src/pages/Splash/index.tsx
@@ -1,55 +1,37 @@
 import React from 'react';
-import { Button , StyleSheet, Dimensions} from 'react-native';
 
+import BrandSvg from '../../assets/brand.svg';
+import LogoSvg from '../../assets/logo.svg';
 
-import Animated, {useSharedValue, useAnimatedStyle,withTiming, Easing} from 'react-native-reanimated';
+import Animated, {
+    useSharedValue, 
+    useAnimatedStyle,
+    withTiming, 
+    Easing
+} from 'react-native-reanimated';
 
 import {
     Container,
 } from './styles';
 
-const WIDTH = Dimensions.get('window').width;
-
 export const Splash = () => {
-    const animation = useSharedValue(0);//compartilhar valores na animação
+    
 
      //para animar o comportamento visual em tela
      //e animateStyle e basicamente um objeto de estilo igual stylesheet
      //eixo x lados eixo y cima baixo
-    const animatedStyles = useAnimatedStyle(()=>{
-        return{
-            transform: [
-                {
-                translateX: withTiming(animation.value,{ 
-                        duration:3000,
-                        easing:Easing.bounce
-                    }
-                    )
-                }
-            ]
-        }
-    });
-   
-
-    const handleAnimationPosition = ()=>{
-        animation.value = Math.random() * (WIDTH - 100);
-    }
 
     return(
         <Container>
 
-            <Animated.View style={[styles.box,animatedStyles]} />
+            <Animated.View>
+                <BrandSvg width={80} height={50} />
+            </Animated.View>
 
-        <Button title="Mover" onPress={handleAnimationPosition} />
+            <Animated.View>
+                <LogoSvg width={180} height={20} />
+            </Animated.View>
 
         </Container>
     );
 }
-
-const styles = StyleSheet.create({
-    box:{
-        width: 100,
-        height: 100,
-        backgroundColor: "red",
-    }
-});
