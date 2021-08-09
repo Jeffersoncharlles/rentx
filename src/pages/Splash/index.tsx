0ad2fd73be9deb1c2618ba496ef4cd71e2e8ec61
Jefferson Charlle <contato@jefferdeveloper.com>
2021-08-08 22:10:52 -0300

    Splash useffect remove opacity

--- src/pages/Splash/index.tsx
+++ src/pages/Splash/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , {useEffect} from 'react';
 
 import BrandSvg from '../../assets/brand.svg';
 import LogoSvg from '../../assets/logo.svg';
@@ -7,7 +7,9 @@ import Animated, {
     useSharedValue, 
     useAnimatedStyle,
     withTiming, 
-    Easing
+    Easing,
+    interpolate,
+
 } from 'react-native-reanimated';
 
 import {
@@ -15,20 +17,39 @@ import {
 } from './styles';
 
 export const Splash = () => {
-    
+    const splashAnimation = useSharedValue(0);
+
+    const brandStyle = useAnimatedStyle(()=>{
+        return  {
+            opacity: splashAnimation.value
+        }
+    });
+
+    const logoStyle = useAnimatedStyle(()=>{
+        return {
+            opacity: splashAnimation.value
+        }
+    });
 
      //para animar o comportamento visual em tela
      //e animateStyle e basicamente um objeto de estilo igual stylesheet
      //eixo x lados eixo y cima baixo
 
+     useEffect(()=>{
+        splashAnimation.value = withTiming(
+            50,
+            {duration: 1000}
+        )
+     },[]);
+
     return(
         <Container>
 
-            <Animated.View>
+            <Animated.View style={brandStyle}>
                 <BrandSvg width={80} height={50} />
             </Animated.View>
 
-            <Animated.View>
+            <Animated.View style={logoStyle}>
                 <LogoSvg width={180} height={20} />
             </Animated.View>
 
