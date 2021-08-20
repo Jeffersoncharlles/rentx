27a9219c350df375895fed3f0205a34d23bc3deb
Jefferson Charlle <contato@jefferdeveloper.com>
2021-08-20 20:25:19 -0300

    tab routes svg

--- src/routes/app.tab.routes.tsx
+++ src/routes/app.tab.routes.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Platform } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useTheme } from 'styled-components';
+
+import HomeSvg from '../assets/home.svg';
+import CarrosSvg from '../assets/carros.svg';
+import PerfilSvg from '../assets/perfil.svg';
 
 
 import { AppStackRoutes } from './app.stack.routes';
@@ -7,25 +13,58 @@ import { Home } from '../pages/Home';
 import { MyCars } from '../pages/MyCars';
 
 
+
 const {Navigator, Screen} = createBottomTabNavigator();
 
 export const AppTabRoutes = () => {
     //jogando as stacks na home eu libero as duas para usar
+    const theme = useTheme()
 
 
+    //activeTintColor cor selecionado
+    //inactiveTintColor cor quando nada ta selecionado
+    //showLabel nao aparecer textos
+    //tabBarIcon acessa a cor definida active ou inactive
+    // para pegar a cor tem que tirar o fill do svg
     return (
-        <Navigator>
+        <Navigator
+            tabBarOptions={{
+                activeTintColor: theme.colors.main,
+                inactiveTintColor: theme.colors.text_details,
+                showLabel:false,
+                style:{
+                    paddingVertical:Platform.OS === 'ios'? 20:0,
+                    height: 78,
+                    backgroundColor:theme.colors.background_primary
+                }
+            }}
+        >
             <Screen 
                 name="Home"
                 component={AppStackRoutes}
+                options={{
+                    tabBarIcon: ({color})=>(
+                        <HomeSvg width={24} height={24} fill={color}/>
+                    )
+                }}
             />
             <Screen 
                 name="Profile"
                 component={Home}
+                options={{
+                    tabBarIcon: ({color})=>(
+                        <PerfilSvg width={24} height={24} fill={color}/>
+                    )
+                }}
             />
             <Screen 
                 name="MyCars"
                 component={MyCars}
+                options={{
+                    tabBarIcon: ({color})=>(
+                        <CarrosSvg width={24} height={24} fill={color}/>
+                    )
+                }}
             />
         </Navigator>
     );
